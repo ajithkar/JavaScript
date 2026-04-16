49034f9c9e6d6c59447f2ce11161c5ed712d4122
Ajithkar <pajithkar@gmail.com>
2026-04-16 22:37:14 +0530

    more answers added

---
+++ JSpracticalAnswers.js
@@ -0,0 +1,72 @@
+// //Q.01
+// let x;
+// console.log(x);
+// x=33;
+// console.log(x);
+
+// //Q.02
+// let num = 5;
+// num++;
+// console.log(num);
+// ++num;
+// console.log(num);
+
+// //Q.03
+// let user = {
+//     name: "Ajith", age: 20
+// };
+// user.age = 21;
+// user.city = "Vavuniya";
+// console.log(user);
+
+// //Q.04
+// let a = 10;
+// let b = "10";
+// console.log(a == b);
+// console.log(a === b);
+
+// //Q.05
+// let n1 = 25, n2 = 5;
+// console.log(n1 + n2);
+
+// //Q.06
+// let item = ["pen", "book"];
+// item.push("bag");
+// item.splice(1,1);
+// console.log(items);
+
+// //Q.07
+// let status = false;
+// console.log(status);
+// status = true;
+// console.log(status);
+
+// //Q.08
+// console.log(20 + 5 * 2);
+// console.log((20 + 5)* 2)
+
+// //Q.09
+// let data = null;
+// data = 5;
+// data = "Hi";
+// console.log(data);
+
+// //Q.10
+// let student = {
+//     name: "Ajith",
+//     subject: ["Math"]
+// };
+// student.subjects.push("Science");
+// .console.log(student.subjects[0], student);
+
+//Q.11
+// let a = true;
+// let b = false;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+
+//Q.12
+let s = "20", n = 5;
+console.log(s + n);
+console.log(Number(s) + n);
